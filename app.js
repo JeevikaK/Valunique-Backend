196e8f055486b3dbc077805bd354789ffc98ce7b
Jeevika Kiran <jeevika.kiran@gmail.com>
2022-08-30 16:23:36 +0530

    second commit

--- app.js
+++ app.js
@@ -12,8 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // routes
-app.get('/', (req, res) => {
-    res.render('login', {title: 'Login'});
+app.post('/', (req, res) => {
+    const { candidate_id, job_id } = req.body;
+    console.log(candidate_id, job_id);
+    const format_candidate_id = /^[0-9]+$/;
+    const format_job_id = /^[A-Z0-9]+$/;
+    if(candidate_id.length==8 && job_id.length == 8 && format_candidate_id.test(candidate_id) && format_job_id.test(job_id))
+        console.log(true);
+    else
+        var message = 'Incorrect format for Candidate ID or Job ID';
+    res.render('login', {title: 'Login', message});
 }); 
 app.get('/details', (req, res) => {
     res.render('details', {title: 'Details'});
