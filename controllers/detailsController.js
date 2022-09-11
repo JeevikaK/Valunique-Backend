e6d245c5f24330533ab66751e3a5a70fda5f3a5a
Jeevika Kiran <jeevika.kiran@gmail.com>
2022-09-11 21:43:43 +0530

    details page done

--- controllers/detailsController.js
+++ controllers/detailsController.js
@@ -11,17 +11,32 @@ const getDetails = async (req, res) => {
         var q1 = req.session.answers['question1']
         var q2 = req.session.answers['question2']
         var location = req.session.answers['location']
-        var Relocate = req.session.answers['location']
+        var relocate = req.session.answers['relocate']
         var skills_mandatory = req.session.answers['Skills']
-        var skill_count = req.session.answers['Skill_Count']
+        var add_skill_list = req.session.answers['add_skills']
 
         const xlData = req.session.xlData;
-        if(q1===undefined || q2===undefined || location===undefined || Relocate===undefined || skills_mandatory==undefined){
-            q1 = q2 = Relocate = '';
+        if(q1===undefined || q2===undefined || location===undefined || relocate===undefined || skills_mandatory==undefined || add_skill_list==undefined){
+            q1 = q2 = relocate = '';
             location = 'Select'
             skills_mandatory = []
+            add_skill_list = []
         }
-        res.render('details', {title: 'Details', mandatorySkills: xlData['mandatorySkills'], jobName, candidate_id: candidateId, message: "", whyVolvo: q1, aboutVolvo: q2, select_location: location, relocate: Relocate, skill_list: skills_mandatory, Skill_Count: skill_count});
+
+        var context = {
+            title: 'Details',
+            mandatorySkills: xlData['mandatorySkills'],
+            jobName,
+            candidate_id: candidateId,
+            message: "",
+            whyVolvo: q1,
+            aboutVolvo: q2,
+            select_location: location,
+            relocate,
+            skill_list: skills_mandatory,
+            add_skill_list
+        }
+        res.render('details', context);
     }
     
 }
@@ -34,30 +49,39 @@ const postDetails = async (req, res) => {
     var add_skill_keys = Object.keys(req.body).filter(key => key.startsWith('add_skill'))
     var skills =''
     var add_skills=''
-    var skill_count = 0
     skill_keys.forEach(skill => {
         skills+= req.body[skill]+', '
-        skill_count++;
     })
-    const skill_list = skills.slice(0, -2).split(", ")
-    console.log(skill_list)
     add_skill_keys.forEach(add_skill =>{
         if(req.body[add_skill]!=='')
             add_skills+= req.body[add_skill]+', '
     })
-    console.log(skills.slice(0, -2), add_skills.slice(0, -2))
+
     var applicant = await Applicant.findOne({ where: { candidateID: candidateId, jobID: jobId, status: 'Applying' }});
     const xlData = req.session.xlData;
     if(q1=='' || q2=='' || location=='' || relocate==''|| skills=='' ){
-        res.render('details', {title: 'Details', mandatorySkills: xlData['mandatorySkills'], jobName, candidate_id: candidateId, message: `<i class="fa fa-exclamation-circle"></i> Please fill all the fields`, value1: q1, value2: q2});
+        var context = {
+            title: 'Details',
+            mandatorySkills: xlData['mandatorySkills'],
+            jobName,
+            candidate_id: candidateId,
+            whyVolvo: q1,
+            aboutVolvo: q2,
+            select_location: location,
+            relocate,
+            skill_list: skills.slice(0, -2).split(", "),
+            add_skill_list: add_skills.slice(0, -2).split(", "),
+            message: `<i class="fa fa-exclamation-circle"></i> Please fill all the fields`
+        }
+        res.render('details', context);
     }
     else{
         req.session.answers['question1'] = q1
         req.session.answers['question2'] = q2
         req.session.answers['location'] = location
         req.session.answers['relocate'] = relocate
-        req.session.answers['Skills'] = skill_list
-        req.session.answers['Skill_Count'] = skill_count
+        req.session.answers['Skills'] = skills.slice(0, -2).split(", ")
+        req.session.answers['add_skills'] = add_skills.slice(0, -2).split(", ")
 
         var updateResult = applicant.update({
             whyVolvo: q1,
